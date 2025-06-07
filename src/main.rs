2ce635cb06f6fa9b9955862ddee6cc2a097a5761
June P <rjp2030@outlook.com>
2025-06-07 19:47:45 +0900

    feat: improve comms

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ type Console = arduino_hal::hal::usart::Usart0<arduino_hal::DefaultClock>;
 static CONSOLE: interrupt::Mutex<RefCell<Option<Console>>> =
     interrupt::Mutex::new(RefCell::new(None));
 
-
 macro_rules! print {
     ($($t:tt)*) => {
         interrupt::free(
@@ -51,10 +50,27 @@ fn main() -> ! {
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
     let my_pin = pins.a0.into_analog_input(&mut adc);
 
-    println!("Hello from main!");
+    // println!("Hello from main!");
+
+    // interrupt::free(|cs| {
+    //     if let Some(console) = CONSOLE.borrow(cs).borrow_mut().as_mut() {
+    //         let _ = console.write_byte(0x66);
+    //         console.write_byte(0x78);
+    //         console.write_byte(0x88);
+    //     }
+    // });
 
     loop {
-        println!("{}", my_pin.analog_read(&mut adc));
+        let dat = my_pin.analog_read(&mut adc).to_le_bytes();
+        interrupt::free(|cs| {
+            if let Some(console) = CONSOLE.borrow(cs).borrow_mut().as_mut() {
+                for d in dat {
+                    let _ = console.write_byte(d);
+                }
+                console.flush();
+            }
+        });
+
         arduino_hal::delay_ms(100);
     }
 }
